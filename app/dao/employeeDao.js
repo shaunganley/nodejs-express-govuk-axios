c93c9c4d2c7be90bf9d2d70a95508e9a384f150e
shaunganley <s.ganley@kainos.com>
2021-11-11 12:30:03 +0000

    add buttons

--- app/dao/employeeDao.js
+++ app/dao/employeeDao.js
@@ -8,7 +8,6 @@ function wrapDB (dbconfig) {
     const pool = mysql.createPool(dbconfig) 
     return { 
         query(sql, args) { 
-            console.log("in query in wrapper") 
             return util.promisify( pool.query ) 
             .call(pool, sql, args) 
         }, 
