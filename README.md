ef63b8eeea74571c1b96d9aee6c6136f3e0287d5
shaunganley <s.ganley@kainos.com>
2021-11-12 10:04:18 +0000

    using axios to call API

--- README.md
+++ README.md
@@ -1,26 +1,10 @@
 # nodejs-express-govuk-mysql
 
-Database
+REST API
 ---
-1. Create an empty database
-1. Run the following command from the base of your cloned directory to create the required database structure:
-```
-mysql --host=<localhost> --user=<your_username> --password=<your_password> <your_database_name> < employeesdb.sql
-```
+This application provides a frontend for the REST API: https://github.com/shaunganley/java-dropwizard-swagger-jdbc
+1. Follow the steps in the above link to run the REST API
 
-Properties file
----
-1. Create a file in the `app` directory called 'dbconfig.json'
-1. Add the following contents to the file:
-```
-{ 
-    "connectionLimit": 10, 
-    "host":     <database host>, 
-    "user":     <username>,
-    "password": <password>,
-    "database": <database name>
-}
-```
 
 How to start the nodejs-express-govuk-mysql application
 ---
--- app/controller/employeeController.js
+++ app/controller/employeeController.js
@@ -1,34 +1,34 @@
 const express = require('express')
 const router = express.Router()
-
-// Add your routes here - above the module.exports line
-const employeeDao = require('../dao/employeeDao')
+const axios = require('axios');
+axios.defaults.baseURL = 'http://localhost:8080/';
 
 router.get('/employees', async (req, res) => { 
-    let results = await employeeDao.getEmployees()
+    let results = await axios.get('/hr/employee')
+    let data = results.data
 
-    for (let i = 0; i < results.length; i++) {
-        results[i].viewUrl = `<a href='employees/${results[i].employeeId}'>View</a>`
+    for (let i = 0; i < data.length; i++) {
+        data[i].viewUrl = `<a href='employees/${data[i].employeeId}'>View</a>`
     }
 
-    res.render('list-employees', { employees: results } ) 
+    res.render('list-employees', { employees: data } ) 
 });
 
 router.get('/employees/:id', async (req, res) => { 
-    let results = await employeeDao.getEmployeeById(req.params.id)
-    res.render('list-employee', { employee: results[0] } ) 
+    let results = await axios.get('/hr/employee/' + req.params.id)
+    res.render('list-employee', { employee: results.data } ) 
 });
 
 router.get('/insert-employee', async (req, res) => { 
     res.render('employee-form') 
 });
 
-router.post('/insert-employee', async (req, res) => { 
+router.post('/insert-employee', async (req, res) => {
     try {
-        let id = await employeeDao.insertEmployee(req.body)
-        res.redirect('/employees/' + id)
+        let response = await axios.post('/hr/employee', req.body)
+        res.redirect('/employees/' + response.data)
     } catch (e) {
-        res.locals.errormessage = e.sqlMessage
+        res.locals.errormessage = "Failed to submit form"
         res.render('employee-form', req.body) 
     }
 });
